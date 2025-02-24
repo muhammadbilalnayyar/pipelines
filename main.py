f6ceae99fe22d37c01c77723945f4fab1b4647ea
Muhammad Bilal Nayyar <40485379+muhammadbilalnayyar@users.noreply.github.com>
2025-02-24 21:07:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Main File")
+from fastapi import FastAPI
+
+# Create an instance of FastAPI
+app = FastAPI()
+
+# Define a route for the root ("/") endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+# Define a route for the "/items/{item_id}" endpoint
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
