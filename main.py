b58ed616db7e64e621f3845d58000794a0f9e3aa
Muhammad Bilal Nayyar <40485379+muhammadbilalnayyar@users.noreply.github.com>
2025-02-24 22:24:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+# This block ensures uvicorn runs only when the script is run directly
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001, reload=True)
